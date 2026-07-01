22dfa55f5f34b12f351701eaf1ec1e8637a01854
June <juneisstare@gmail.com>
2026-07-01 11:32:40 +0700

    fixing exception to send http error with status 500

--- dummy/api_hit.py
+++ dummy/api_hit.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import asyncio
 import random
 
@@ -9,5 +9,5 @@ async def ping():
     # Simulate a random delay with log-normal distribution ~81% below 500 ms, ~96% below 2 seconds
     await asyncio.sleep(random.lognormvariate(-2, 1.5))
     if random.random() < 0.005:  # 0.5% chance to simulate an error
-        raise Exception("Simulated error")
+        raise HTTPException(status_code=500, detail="Simulated server error")
     return {"status": "ok"}
