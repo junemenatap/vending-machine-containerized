cc61d8733b83eeeef718b8e4bb5627f7fa3fc085
June <juneisstare@gmail.com>
2026-07-01 09:35:31 +0700

    altered dummy delay time to closely simulate a real api hit delay with lognorm distribution 2

--- dummy/api_hit.py
+++ dummy/api_hit.py
@@ -6,8 +6,8 @@ app = FastAPI()
 
 @app.get("/ping")
 async def ping():
-    # Simulate a random delay with log-normal distribution ~75% below 500 ms, ~91% below 2 seconds
-    await asyncio.sleep(random.lognormvariate(-2.2, 2.2))
+    # Simulate a random delay with log-normal distribution ~81% below 500 ms, ~96% below 2 seconds
+    await asyncio.sleep(random.lognormvariate(-2, 1.5))
     if random.random() < 0.005:  # 0.5% chance to simulate an error
         raise Exception("Simulated error")
     return {"status": "ok"}
