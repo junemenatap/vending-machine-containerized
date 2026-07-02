025ec3d818808d044a8f3e5651f0a4413b72b8a5
June <juneisstare@gmail.com>
2026-07-02 10:32:34 +0700

    small fixes

---
+++ testscripts/raceslow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+item=${1:-test}
+pps=${2:-100}
+mins=${3:-2}
+total=$((mins * 60))
+end=$((SECONDS + total))
+
+while [ $SECONDS -lt $end ]; do
+    for i in {1..$pps}; do
+        curl -s -X POST "http://localhost:8000/products/buy/${item}" > /dev/null &
+    done
+    wait
+done
+
+wait
